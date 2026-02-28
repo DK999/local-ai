0035f66967985580db694c676f3ec20523a161f4
DK999 <eldk999@gmail.com>
2026-02-28 08:29:10 +0100

    Add deepseek-r1 as example LLM

--- scripts/pull-ollama-models.sh
+++ scripts/pull-ollama-models.sh
@@ -138,6 +138,7 @@ else
     DEFAULT_MODELS=(
       "qwen2.5-coder:14b-instruct-q5_K_M" # Chat-Model for Open-WebUI
       "deepseek-coder-v2:latest" # or :lite to pin Q4_0, Chat-Model for Open-WebUI
+      "deepseek-r1:32b-q3_K_M" # Chat model optimized for coding
       "qwen3-embedding:0.6b" # Embedding model - Convert text into numerical vectors so you can compare meaning, not just words.
       "dengcao/qwen3-reranker-0.6b:q8_0" # Reranker modek - Improve search or RAG results by scoring the relevance of (query, document) pairs with higher accuracy than embeddings.
       "qwen2.5-coder:1.5b-base" # Autocomplete model - Predict the next tokens for fast inline typing completion.
@@ -147,6 +148,7 @@ else
     DEFAULT_MODELS=(
       "qwen2.5-coder:14b-instruct-q4_K_M" # Chat-Model for Open-WebUI
       "deepseek-coder-v2:latest" # or :lite to pin Q4_0, Chat-Model for Open-WebUI
+      "deepseek-r1:14b-q8_0" # Chat model optimized for coding
       "qwen3-embedding:0.6b" # Embedding model - Convert text into numerical vectors so you can compare meaning, not just words.
       "dengcao/qwen3-reranker-0.6b:q8_0" # Reranker modek - Improve search or RAG results by scoring the relevance of (query, document) pairs with higher accuracy than embeddings.
       "qwen2.5-coder:1.5b-base" # Autocomplete model - Predict the next tokens for fast inline typing completion.
